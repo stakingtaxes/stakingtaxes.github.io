247b4a8386c143c1312021874cc24df348e565da
Staking Taxes <1212hammersmith@gmail.com>
2023-09-24 14:32:04 +0800

    docs: add staking docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Staking Taxes</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/stakingtaxes/stakingtaxes.github.io',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/stakingtaxes/stakingtaxes.github.io',
   footer: {
-    text: 'Nextra Docs Template',
+    component: null,
   },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Staking Taxes'
+    }
+  }
 }
 
 export default config
