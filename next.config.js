dff0dd8f767d6c37d27510a039220b77dde0373a
Staking Taxes <1212hammersmith@gmail.com>
2023-09-24 14:45:50 +0800

    docs: upload build for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+/** @type {import('next').NextConfig} */
 
-module.exports = withNextra()
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = withNextra(nextConfig);
